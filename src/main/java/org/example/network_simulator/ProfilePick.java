de3bf88d4e20814a05cb01c556634d5cd08d5dc2
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-05-03 15:36:07 -0400

    added todos in ProfilePick class, also profile-pick layout

---
+++ src/main/java/org/example/network_simulator/ProfilePick.java
@@ -0,0 +1,21 @@
+package org.example.network_simulator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class ProfilePick {
+
+    @FXML
+    private Button backBtn,continueBtn,editPhotoBtn;
+    @FXML
+    private ImageView image;
+
+
+    //todo - initialize() here
+    //todo - back btn (initialize) should go back
+    //todo - continue btn (continueBtn) should take the pick, save to the database.
+    //todo - edit photo (editPhotoBtn) btn should be able to re upload the photo.
+    //todo - load the image (ImageView image) from the user database before switching to the new scene.
+
+}
